e60a3cd62a01bdfe9648e43bade27b64078b1710
TheKangChen <kangchen.work@gmail.com>
2021-11-04 02:51:12 -0400

    Reorder functions to alphabetic order

--- Modules/audio.py
+++ Modules/audio.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal.signaltools import deconvolve
 
 class AudioEffects():
     speed = 343.3 # Speed of sound at 20 degree celcius
@@ -7,6 +8,10 @@ class AudioEffects():
         self.samplerate = fs
 
 
+    def amp_to_db(self, x:float):
+        return 20 * np.log10(x)
+    
+
     def attenuation(self, x:np.array, ratio:float):
         if ratio > 1:
             raise ValueError('Ratio must be smaller than 1')
@@ -14,18 +19,26 @@ class AudioEffects():
         return x * ratio
 
 
-    def invert(self, x:np.array):
-        return x * -1
+    def combine_channels(self, *args:np.array):
+        '''
+        Output channel index = Order of input
+        '''
+        output = []
+        for channel in args:
+            output.append(channel)
+        output = np.vstack(output)
 
+        return output
+    
 
     def delay(self, x:np.array, sec:float):
         delay_samples = int(np.floor(sec * self.samplerate))
 
         return np.concatenate((np.zeros(delay_samples),x))
+    
 
-
-    def amp_to_db(self, x:float):
-        return 20 * np.log10(x)
+    def invert(self, x:np.array):
+        return x * -1
 
 
     def sum_audio(self, *args:np.array):
